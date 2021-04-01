bfbc841197a0dcf8c52e2bb1cc2557757018e968
miche <miche@DESKTOP-54D2MNI>
2021-03-31 23:12:44 -0300

    commit inicial02

---
+++ Exercicios/src/aula02/DadosPessoais.java
@@ -0,0 +1,22 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class DadosPessoais{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o seu nome: ");		
+		String nome = sc.nextLine();
+		System.out.println("Seu nome é: " + nome);
+		
+		System.out.print("Digite um idade: ");
+		int idade = sc.nextInt();
+		System.out.println("idade: " + idade );
+		
+		System.out.print("Digite sua altura: " );
+		double altura = sc.nextDouble();
+		System.out.println("altura: " + altura);
+		System.out.println("Nome: " + nome +  " - Idade: " + idade +  " "
+				+ "- Altura: "+  altura );
+}
+}
---
+++ Exercicios/src/aula02/VariaveisNumericas.java
@@ -0,0 +1,19 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class VariaveisNumericas {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um inteiro: ");
+		int num = sc.nextInt();
+		System.out.println("Digite um número coom decimais: ");
+		double dec = sc.nextDouble();
+		System.out.println("Inteiro = " + num + " \n " + " Decimal = " + dec);
+		
+		
+		
+		
+	}
+
+}
